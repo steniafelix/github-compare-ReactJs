709af698d8554c84030031699a02a55157d1522d
Stenia Felix <steniafelix@gmail.com>
2019-08-13 20:19:49 -0300

    Implementing main page

---
+++ src/pages/Main/index.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import api from "../../services/api";
+
+import logo from "../../assests/logo.png";
+
+import { Container, Form } from "./styles";
+import CompareList from "../../components/CompareList";
+
+export default class Main extends Component {
+  state = {
+    repositoryInput: "",
+    repositories: []
+  };
+
+  handleAddRepository = async e => {
+    e.preventDefault();
+
+    try {
+      const response = await api.get(`repos/${this.state.repositoryInput}`);
+
+      this.setState({
+        repositoryInput: "",
+        repositories: [...this.state.repositories, response.data]
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  render() {
+    return (
+      <Container>
+        <img src={logo} alt="Github Compare" />
+
+        <Form onSubmit={this.handleAddRepository}>
+          <input
+            type="texte"
+            placeholder="usuário/repositório"
+            value={this.state.repositoryInput}
+            onChange={e => this.setState({ repositoryInput: e.target.value })}
+          />
+          <button type="submit">OK</button>
+        </Form>
+
+        <CompareList repositories={this.state.repositories} />
+      </Container>
+    );
+  }
+}
