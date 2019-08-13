5d5fde94268ea8b83261adb279d4c8a79057f638
Stenia Felix <steniafelix@gmail.com>
2019-08-13 20:17:44 -0300

    Setting up styles

---
+++ src/styles/global.js
@@ -0,0 +1,18 @@
+import { injectGlobal } from "styled-components";
+
+injectGlobal`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0
+  }
+
+  body {
+    background: #9B65E6;
+    text-rendering: optimizeLegibility !important;
+    -webkit-font-smoothing: antialiased !important;
+    font-family: sans-serif;
+  }
+
+`;
